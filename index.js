1e9476391438dec36560a4e8b9de0f33d82999db
mcumm64 <mcumm64@gmail.com>
2019-04-23 21:22:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,32 +1,64 @@
 
-var katzDeliLin = [];
-// the current line of people
-function takeANumber (katzDeliLine, name) {
-  katzDeliLine.push(name);
-return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`; 
+// function takeANumber(line, name) {
+//   line.push(name)
+//   return `Welcome, ${name}. You are number ${line.indexOf(name)+1} in line.`
+// }
+
+// function nowServing(line) {
+//   if (line.length >=1) {
+//     return `Currently serving ${line.shift()}.`
+//   } else {
+//     return "There is nobody waiting to be served!"
+//   }
+// }
+  
+// function currentLine(line) {
+//   	var line_list = line.map(x => (` ${line.indexOf(x)+1}. ${x}`))
+//   	if (line.length >= 1) {
+// 	return `The line is currently:${line_list}`
+// 	} else {
+// 	return "The line is currently empty."
+// 	}
+// }
+
+// function uninterruptedService(line) {
+// 	if (!line.length) {
+// 		return "The line is currently empty"
+//     } else {
+//         console.log(currentLine(line))
+// 		    for (var i = line.length; i > -1; i--) {
+// 	 	  	  console.log(nowServing(line))
+// 		  	  if (line.length) {
+// 				  console.log(currentLine(line))
+// 			  }
+// 		  }
+//     }
+// }
+
+var katzLine = []
+
+function takeANumber(line, name) {
+  line.push(name)
+  return `Welcome, ${name}. You are number ${line.indexOf(name)+1} in line.`
 }
 
-function nowServing (katzDeliLine) {
-  if (katzDeliLine.length !== 0) {
-    var firstInLine = katzDeliLine[0];
-  katzDeliLine.shift();
-return `Currently serving ${firstInLine}.`;
+function nowServing(line) {
+  if (line.length >=1) {
+  return `Currently serving ${line.shift()}.`
   } else {
-    return "There is nobody waiting to be served!";
+    return "There is nobody waiting to be served!"
   }
 }
 
-function currentLine (array) {
-  // if loop numerates elements starting with 1. (1. index0)
-  var testArray = [];
-  if (array.length) {
-    for (var i = 0; i < array.length; i++) {
-      testArray.push(` ${i+1}. ${array[i]}`);
+function currentLine(line) {
+  var orderedLine = []
+  if (line.length >=1) {
+    for (var i = 0; i<line.length; i++) {
+      orderedLine.push(` ${[i+1]}. ${line[i]}`)
     }
-    var numsAndNames = "The line is currently:";
-    return numsAndNames + testArray;
+  return `The line is currently:${orderedLine}`
   } else {
-    var emptyLine ="The line is currently empty.";  
-    return emptyLine;
+    return "The line is currently empty."
   }
 }
+
